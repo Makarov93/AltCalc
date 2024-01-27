d3256ed1a7efaf5f98a2216a743aff04b095800d
Makarov93 <153182603+Makarov93@users.noreply.github.com>
2024-01-27 22:51:05 +0300

    AlrCalc Update

--- AltCalc/src/Main.java
+++ AltCalc/src/Main.java
@@ -24,7 +24,6 @@ public class Main {
             default:
                 throw new IllegalArgumentException("Неверный оператор. Поддерживаемые операторы: +, -, *, и /.");
         }
-
         System.out.println(result);
 
     }
